891229efc91427bd24eda0e87a6684f42067c176
Douglas DuBois <douglas.dubois1@gmailcom>
2021-09-22 18:03:45 -0500

    more email formatting

--- send_email.py
+++ send_email.py
@@ -10,13 +10,13 @@ email_from = os.getenv('EMAIL_FROM')
 email_to = os.getenv('EMAIL_TO')
 password = os.getenv('PASSWORD')
 
-def send_email(name, url, subject="", condition="", error=""):
+def send_email(name, url, subject='', condition='', error=''):
     message = EmailMessage()
     
-    message.add_header('Subject', subject if subject == "Error" else f"({condition}) {name}")
+    message.add_header('Subject', f'(Error) {name}' if subject == 'Error' else f'({condition}) {name}')
     message.add_header('From', email_from)
     message.add_header('To', email_to)
-    message.set_content(f"Product: {name}\nError: {error}\nURL: {url}" if subject == "error" else f'Product: {name}\nURL: {url}')
+    message.set_content(f'Product: {name}\nURL: {url}\nError: {error}' if subject == 'error' else f'Product: {name}\nURL: {url}')
     
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=ssl.create_default_context()) as server:
         server.login(email_from, password)
